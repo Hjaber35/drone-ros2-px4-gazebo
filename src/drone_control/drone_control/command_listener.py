cbb9ff8b44b8b70c5d7c52b75c016fa294d1901f
Hjaber35 <Jhcjaber@hotmail.com>
2026-05-11 23:04:58 -0400

    Add command handler logic

--- src/drone_control/drone_control/command_listener.py
+++ src/drone_control/drone_control/command_listener.py
@@ -16,8 +16,42 @@ class CommandListener(Node):
         self.get_logger().info("Command listener started.") 
 
     def command_callback(self, msg):
+        command = msg.data
+
         self.get_logger().info(f"Received command: {msg.data}")
 
+        if command == "TAKEOFF":
+            self.handle_takeoff()
+ 
+        elif command == "FORWARD":
+            self.handle_forward()
+        
+        elif command == "LEFT":
+            self.handle_left()
+         
+        elif command == "RIGHT":
+             self.handle_right()
+ 
+        elif command == "LAND":
+             self.handle_land()
+        
+        else:
+            self.get_logger().warn(f"Uknown command: {command}")
+
+    def handle_takeoff(self):
+        self.get_logger().info("Action: Drone should take off.")
+
+    def handle_forward(self):
+        self.get_logger().info("Action: Drone should move forward.")
+
+    def handle_left(self):
+        self.get_logger().info("Action:Drone should turn or move left.")
+  
+    def handle_right(self):
+        self.get_logger().info("Action:Drone should turn or move right.")
+    
+    def handle_land(self):
+        self.get_logger().info("Action:Drone should land.")
 
 def main(args=None):
     rclpy.init(args=args)
--- src/drone_control/drone_control/command_publisher.py
+++ src/drone_control/drone_control/command_publisher.py
@@ -1,26 +1,43 @@
 import rclpy
-from rclpy.node import Node 
-from std_msgs.msg import String 
+from rclpy.node import Node
+from std_msgs.msg import String
 
 
-class CommandPublisher(Node): 
+class CommandPublisher(Node):
     def __init__(self):
         super().__init__("command_publisher")
+
         self.publisher = self.create_publisher(
             String,
             "drone_command",
-            10 //ROS can keep a small buffer of messages if needed
+            10
         )
 
-        self.timer = self.create_timer(1.0, self.send_command)
+        self.commands = [
+            "TAKEOFF",
+            "FORWARD",
+            "LEFT",
+            "RIGHT",
+            "LAND"
+        ]
+        self.command_index = 0
+        self.timer = self.create_timer(2.0, self.send_command)
 
         self.get_logger().info("Command publisher started.")
-    def send_command(self): 
-        msg = String() 
-        msg.data = "TAKEOFF" 
-        self.publisher.publish(msg) 
+
+    def send_command(self):
+        msg = String()
+
+        msg.data = self.commands[self.command_index]
+
+        self.publisher.publish(msg)
 
         self.get_logger().info(f"Published command: {msg.data}")
+
+        self.command_index += 1
+
+        if self.command_index >= len(self.commands):
+            self.command_index = 0
 
 
 def main(args=None):
